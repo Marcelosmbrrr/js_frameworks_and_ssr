b9dd4c286f2f699ec8c5f492abcb69d68d179552
Marcelo Moreira <profissionalsmbr@gmail.com>
2022-11-18 16:47:20 -0300

    react app layout structure

--- react_set/essential/src/views/LayoutView.js
+++ react_set/essential/src/views/LayoutView.js
@@ -5,16 +5,41 @@ export function LayoutView() {
     return (
         <div className={styles.container}>
             <aside className={styles.aside}>
-                aside
+                <div className={styles.nav_icons}>
+                    <ul>
+                        <li>ICON</li>
+                        <li>ICON</li>
+                        <li>ICON</li>
+                        <li>ICON</li>
+                    </ul>
+                </div>
+                <div className={styles.nav_text}>
+                    <ul>
+                        <li>TEXT</li>
+                        <li>TEXT</li>
+                        <li>TEXT</li>
+                        <li>TEXT</li>
+                    </ul>
+                </div>
             </aside>
             <header className={styles.header}>
-                header
+                <div className={styles.header_left}>
+                    HEADER LEFT
+                </div>
+                <div className={styles.header_right}>
+                    HEADER RIGHT
+                </div>
             </header>
             <section className={styles.section}>
-                section
+                <div className={styles.section_top}>
+                    SECTION TOP
+                </div>
+                <div className={styles.section_middle}>
+                    SECTION MIDDLE
+                </div>
             </section>
             <footer className={styles.footer}>
-                footer
+                FOOTER
             </footer>
         </div>
 
