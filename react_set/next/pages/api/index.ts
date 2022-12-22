71a467e5babdbad50938739b009ff296d593efe7
Marcelo Moreira <profissionalsmbr@gmail.com>
2022-12-21 23:14:38 -0300

    react next - login frontend and _document config

--- react_set/next/pages/api/index.ts
+++ react_set/next/pages/api/index.ts
@@ -1,13 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+/*
+Documentation: https://nextjs.org/docs/api-routes/introduction
+
+In essence, Next. js API Routes eliminate the need for creating an additional backend server in your full-stack web applications. 
+With Next. js API Routes, you can access or store data in your database like you would if you were using a separate backend application
+*/
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: 'Name Example' });
 }
